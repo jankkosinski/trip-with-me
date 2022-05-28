572a246235e61a400822fc27af6122d1dfd4f054
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-05-28 17:30:54 +0200

    🚚 ceate basic routings + add react-router-dom V6 package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>TrimWithMe</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
