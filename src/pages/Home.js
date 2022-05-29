894e83a2c9dfac0a8120f310fb56a6f07d45ff68
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-05-29 23:40:33 +0200

    💄 button styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Header>
-        <button>Get Started</button>
+        <button className='btn-default'>Get Started</button>
       </Header>
     </>
   );
