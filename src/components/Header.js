213fefe088de197323eb08d70d95a5e7cb9bc24f
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-05-29 21:27:40 +0200

    🎨  add polygon to video

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 export default function Header() {
+  const video_path = '/assets/videos/header.mp4';
+  const poster_path = '/assets/images/header_image.png';
   return (
     <div className='header'>
       <video
         className='header__video'
-        src={process.env.PUBLIC_URL + '/assets/videos/header.mp4'}
+        poster={process.env.PUBLIC_URL + poster_path}
+        src={process.env.PUBLIC_URL + video_path}
         autoPlay
         loop
         muted
