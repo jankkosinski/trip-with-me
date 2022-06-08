a8d7e8379b103dd0e07a577fa4653aac17a60c65
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-06-08 17:09:35 +0200

    💄 update navigation

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
   return (
     <>
       <div className='nav'>
         <div className='nav__content'>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
+          <NavLink className='nav__content__link' to='/'>
+            Home
+          </NavLink>
+          <NavLink className='nav__content__link' to='/about'>
+            About
+          </NavLink>
         </div>
       </div>
     </>
