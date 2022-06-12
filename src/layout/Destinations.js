5513b349e493100001fd73a9fddedd88a714bfdb
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-06-12 20:00:15 +0200

    ✨ get data from destinations context

--- src/layout/Destinations.js
+++ src/layout/Destinations.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import DestinationsContext from '../context/DestinationsContext';
 
 export default function Destinations() {
   const title = 'Discover story-worthy travel moments - Trip With Me!';
   const description =
     'The results are in! Explore this year’s expert-approved list of must-see destinations, places, and unforgettable experiences guaranteed to inspire. Whether you’re interested in traveling to a new city, going on a cruise, or cooking a new dish — we’re committed to inspiring you to experience travel in a whole new way. ';
+  const {
+    state: { destinations }, // The object destructuring in destructuring process!! - interesting solution to get data
+    dispatch,
+  } = useContext(DestinationsContext);
   return (
     <div className='destinations'>
       <div className='container'>
@@ -14,6 +19,11 @@ export default function Destinations() {
           <div className='col-6 destinations__description'>
             <p className='destinations__description__body'>{description}</p>
           </div>
+        </div>
+        <div className='row destinations__main'>
+          {destinations.map((obj) => (
+            <img className='col-3' src={obj.image} />
+          ))}
         </div>
       </div>
     </div>
