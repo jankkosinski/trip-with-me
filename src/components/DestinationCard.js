cd0cd866159505e4982d8036e5f5f03b604ac887
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-06-13 19:20:57 +0200

    ⚡️ add lazy loading images + add package

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 export default function DestinationCard({ destination }) {
   return (
     <div className='col-3'>
       <div className='destinations__card'>
-        <img
+        <LazyLoadImage
           className='destinations__card__img'
           alt={destination.image}
           src={destination.image}
