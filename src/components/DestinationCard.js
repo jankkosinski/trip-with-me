cb90f1ffb43437d3a1e89b8ce1bdfa9265cbc3c8
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-06-13 22:18:58 +0200

    💄 gradient layout for images + change styles for destination

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 export default function DestinationCard({ destination }) {
   return (
-    <div className='col-3'>
-      <div className='destinations__card'>
+    <div className='col-3 destination'>
+      <div className='destination__card'>
         <LazyLoadImage
-          className='destinations__card__img'
+          className='destination__card__img'
           alt={destination.image}
           src={destination.image}
         />
+        <div className='destination__card__layer'></div>
       </div>
     </div>
   );
