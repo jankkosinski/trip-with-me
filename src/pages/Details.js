795931e762b2dd9429069d9c5637bb6d56118c85
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-06-18 12:12:58 +0200

    🧐 find details object for specific details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import DestinationsContext from '../context/DestinationsContext';
 
 export default function Details() {
-  return <div>Details</div>;
+  const { id } = useParams();
+  const {
+    state: { destinations },
+  } = useContext(DestinationsContext);
+  const details = destinations.find(
+    (destination) => destination.id === parseInt(id)
+  );
+  console.log(details);
+  return <div>{details.name}</div>;
 }
