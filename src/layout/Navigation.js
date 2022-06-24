36c092b066aa95cc83a1344e73ef07d04c95854d
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-06-24 14:07:28 +0200

    🐛 navigation onClick fix closeing navbar

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,17 +1,26 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import NavContext from '../context/NavContext';
+import { NAV_TOGGLE } from '../context/types/NavTypes';
 
 export default function Navigation() {
-  const { state } = useContext(NavContext);
+  const { state, dispatch } = useContext(NavContext);
   return (
     <>
       <div className={`nav ${state ? '' : 'nav--closed'}`}>
         <div className={`nav__content ${state ? '' : 'nav__content--closed'}`}>
-          <NavLink className='nav__content__link' to='/'>
+          <NavLink
+            className='nav__content__link'
+            onClick={() => dispatch({ type: NAV_TOGGLE })}
+            to='/'
+          >
             Home
           </NavLink>
-          <NavLink className='nav__content__link' to='/about'>
+          <NavLink
+            className='nav__content__link'
+            onClick={() => dispatch({ type: NAV_TOGGLE })}
+            to='/about'
+          >
             About
           </NavLink>
         </div>
