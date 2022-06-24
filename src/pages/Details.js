9c3a4654e01ddcd297bdaf6c3c4ce3788b38b314
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-06-24 15:19:52 +0200

    🏗️  header for details + improve header component to overwrite params

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import DestinationsContext from '../context/DestinationsContext';
+import Header from '../layout/Header';
 
 export default function Details() {
   const { id } = useParams();
@@ -11,5 +12,15 @@ export default function Details() {
     (destination) => destination.id === parseInt(id)
   );
   console.log(details);
-  return <div>{details.name}</div>;
+  return (
+    <>
+      <Header
+        title={details.name}
+        intro_text={''}
+        video_path={''}
+        poster_path={details.bigImage}
+        logo={''}
+      ></Header>
+    </>
+  );
 }
