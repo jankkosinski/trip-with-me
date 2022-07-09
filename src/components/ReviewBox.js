6dbfd5c7a1a550a211186e108d966ba90aa262d0
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-10 01:16:59 +0200

    🔨 create reviews component + add review box component

---
+++ src/components/ReviewBox.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function ReviewBox({ data }) {
+  console.log(data);
+  return <div>ReviewBox</div>;
+}
--- src/layout/sections/Reviews.js
+++ src/layout/sections/Reviews.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import ReviewBox from '../../components/ReviewBox';
 
 export default function Reviews({ reviews }) {
-  return <div>Reviews</div>;
+  const _title = 'Reviews';
+  return (
+    <div className='reviews'>
+      <div className='container'>
+        <div className='details__subheader'>{_title}</div>
+        <div className='row'>
+          {reviews.map((review) => (
+            <div className='col-3' key={review.id}>
+              <ReviewBox data={review}></ReviewBox>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
