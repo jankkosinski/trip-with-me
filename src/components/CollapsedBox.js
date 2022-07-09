c150be79cfa63350aa6c12a8315ed5287945ec3f
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-09 16:26:59 +0200

    ✨ collapsing logic

--- src/components/CollapsedBox.js
+++ src/components/CollapsedBox.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { BiChevronRight } from 'react-icons/bi';
+import React, { useState } from 'react';
+import { BiChevronDown, BiChevronRight } from 'react-icons/bi';
 
 export default function CollapsedBox({ header, body }) {
+  const [collapsed, setCollapsed] = useState(1);
   return (
     <div className='collapse-box'>
-      <div className='collapse-box__heading'>
-        <BiChevronRight size={'3rem'}></BiChevronRight>
+      <div
+        className='collapse-box__heading'
+        onClick={() => setCollapsed((prevState) => !prevState)}
+      >
+        {collapsed ? (
+          <BiChevronRight size={'3rem'}></BiChevronRight>
+        ) : (
+          <BiChevronDown size={'3rem'}></BiChevronDown>
+        )}
+
         {header}
       </div>
-      <div className='collapse-box__body'>{body}</div>
+      {collapsed ? null : <div className='collapse-box__body'>{body}</div>}
     </div>
   );
 }
