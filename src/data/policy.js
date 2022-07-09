a5fafd3239db6d78d3c4fe7ec1c8a252d9c78149
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-09 13:28:12 +0200

    🤡 add policy mocks data

---
+++ src/data/policy.js
@@ -0,0 +1,27 @@
+export const policy = [
+  {
+    id: 1,
+    header: 'General provisions',
+    p: 'By adding a comment, the User accepts the conditions set forth in the Regulations and agrees to comply with the obligations under the Regulations created by TWM (Trip With Me).',
+  },
+  {
+    id: 2,
+    header: 'Rules of use of the Forum',
+    p: 'Users are obliged to use the Forum in accordance with the provisions of the Regulations and applicable laws.',
+  },
+  {
+    id: 3,
+    header: 'Copyright',
+    p: 'In exchange for providing the service of enabling the use of the Forum, the User authorizes TWM to use the Work without time, territorial or quantitative limitations, in all known fields of exploitation.',
+  },
+  {
+    id: 4,
+    header: 'Content',
+    p: 'Users are obliged to publish on the Forum only content that is consistent with the topic of the commented material.',
+  },
+  {
+    id: 5,
+    header: 'Final provisions',
+    p: 'TWM nie odpowiada za treść wypowiedzi publikowanych na Forum.',
+  },
+];
