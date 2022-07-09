0bb8e8c4165635cc3bacfd3255e8563980c28bfe
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-09 16:15:59 +0200

    💄 add styles for collapsed section with boxes

--- src/components/CollapsedBox.js
+++ src/components/CollapsedBox.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BiChevronRight } from 'react-icons/bi';
 
 export default function CollapsedBox({ header, body }) {
   return (
     <div className='collapse-box'>
-      <div className='collapse-box__heading'>{header}</div>
+      <div className='collapse-box__heading'>
+        <BiChevronRight size={'3rem'}></BiChevronRight>
+        {header}
+      </div>
       <div className='collapse-box__body'>{body}</div>
     </div>
   );
