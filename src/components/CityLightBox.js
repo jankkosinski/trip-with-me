79bc1bdd9b224f13f86b58d6f979c53e37ff8043
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-12 00:44:38 +0200

    ✨ create basic lightbox component with basic styles

---
+++ src/components/CityLightBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CityLightBox() {
+  return (
+    <div className='city-lightbox'>
+      <div className='city-lightbox__content'>CityLightBox</div>
+    </div>
+  );
+}
