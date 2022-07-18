06057a473f644df4d04efc4a6ed9ce9232c2992d
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-19 00:35:35 +0200

    🚧 add link to github in copyrights

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,8 +4,11 @@ export default function Footer() {
   const _logo = '/assets/icons/logo.png';
   const _heading_text =
     'Remember to do what you love. TripWithMe is an innovative site for travelers where you can find memories and inspiration from others or discover something for yourself. The design of the site was realized in the form of a course that allows you to implement yourself in building a React application structure. The project was prepared with passion and fierceness.';
-  const _copyrights =
-    'TripWithMe, Inc., an not existing company. All rights are not reserved. TripWithMe are not trademarks and not registered anywhere. Project was created by . Greeting to all vitisiors!';
+  const _copyrights = `TripWithMe, Inc., an not existing company. All rights are not reserved. TripWithMe are not trademarks and not registered anywhere. Project was created by `;
+  const _owner_link = 'https://github.com/jankkosinski';
+  const _owner_name = 'Jan Kosiński';
+  const _greetings = '. Greetings to all visitors!';
+
   return (
     <div className='footer'>
       <div className='container'>
@@ -22,7 +25,10 @@ export default function Footer() {
           <div className='footer__copyrights'>
             <div className='footer__copyrights__text'>
               {' '}
-              &copy; {new Date().getFullYear()} {_copyrights}
+              &copy;
+              {new Date().getFullYear()} {_copyrights}
+              <a href={_owner_link}>{_owner_name}</a>
+              {_greetings}
             </div>
           </div>
         </div>
