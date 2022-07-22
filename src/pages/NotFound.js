9df7e10ff1c617153d3d8637315864b42c1c21f6
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-22 09:56:34 +0200

    🚀 create not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Footer from '../layout/Footer';
+import Header from '../layout/Header';
 
 export default function NotFound() {
+  const _title = 'Oops, 404';
+  const _intro_text = `This page does not exist, please follow the navigation to reach our content!`;
+  const _image = '/assets/images/404.jpg';
   return (
     <>
       <Helmet>
         <title>Not Found</title>
       </Helmet>
-      <h1>Not Found</h1>;<Footer></Footer>
+      <Header
+        title={_title}
+        intro_text={_intro_text}
+        video_path={''}
+        poster_path={_image}
+      ></Header>
+      <Footer></Footer>
     </>
   );
 }
