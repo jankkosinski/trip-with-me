953356e8e18b1ef3472d9e0ed3de0c9fb85d8485
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-22 10:16:49 +0200

    🚧 add header and footer + simple content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import Footer from '../layout/Footer';
+import Header from '../layout/Header';
 
 export default function Contact() {
-  return <div>Contact</div>;
+  const _title = 'Contact';
+  const _intro_text = `Fell free to contact us if you have any specific questions or topics! We will help you as much as we can!`;
+  return (
+    <>
+      <Helmet>
+        <title>{_title}</title>
+      </Helmet>
+      <Header title={_title} intro_text={_intro_text}></Header>
+      <Footer></Footer>
+    </>
+  );
 }
