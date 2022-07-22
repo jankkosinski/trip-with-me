caca1df007401347d9b1e3dfcc5cb7c3596d091e
Jan Kosinski <jan.k.kosinski@gmail.com>
2022-07-22 14:04:39 +0200

    💫 animate details page

--- src/components/DetailsBasicsBox.js
+++ src/components/DetailsBasicsBox.js
@@ -4,8 +4,8 @@ export default function DetailsBasicsBox({ title, body }) {
   return (
     <div className='col-6'>
       <div className='details__basics'>
-        <div className='details__basics__title'>{title}</div>
-        <div className='details__basics__body'>{body}</div>
+        <div className='details__basics__title animation'>{title}</div>
+        <div className='details__basics__body animation'>{body}</div>
       </div>
     </div>
   );
